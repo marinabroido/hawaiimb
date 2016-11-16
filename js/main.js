726def2485cdebd341545248fd677eedeb6f0d1f
marinabroido <marinabroido@gmail.com>
2016-11-15 17:30:39 -0800

    JQuery-hide
    Used hide (jQuery) to close cards

--- js/main.js
+++ js/main.js
@@ -1,28 +1,69 @@
 // 1.When user opens the site in the browser hide .hCard, vCard, .imgTitle
+/* done in css: .hero{
+	display:none;
+}
+*/
+
 // 2.When user hovers .heroLeft show  .heroLeft h3
 // 3.When user hovers .heroRight show  .heroright h3
+
+/*  done in css:
+.image-wrapper:hover .content {
+  opacity: 1;
+}
+*/
+
 // 4.When user cliks on .heroLeft:
-//	- hide h3
-//	- show (expand) hCard
+/*	- hide h3
+	- hide .heroLeft
+	- hide .heroRight
+	- hide .hero
+- show (expand) hCard
+*/
 
-$('heroLeft').on('click', function () {
-	$('h3').hide();
-	$('hCard').fadeIn(350);	
+$(document).ready(function(){
 
+$(".heroLeft").on('click',function () {
+	$(".hero").hide();	
+	$(".hCard").fadeIn(350);	
 	
 });
-//5.When user cliks on .heroRight:
-//		-hide h3
-//		-show (expand) vCard
-$('heroRight').on('click', function () {
-	$('h3').hide();
-	$('vCard').fadeIn(350);	
 });
 
-//6. When .cards is open:
+// 5. When .cards is open:
+
+/* When the user clicks the x icon - class .close the hCard fades out*/
+$('.close').on('click', function () {
+	$('.hCard').fadeOut('fast');
+	// Type code related to event here! :)
+	
+});
+
 // - when user clicks hCard button go to page "Wilderness".
 // - when user clicks on x icon colapse .cards.
-//When user click on page "Wilderness":
-//- hide .hero
-//- show main.
+
+
+
+
+
+//6.When user cliks on .heroRight:
+//		-hide h3
+//		-show (expand) vCard
+// $('heroRight').on('click', function () {
+// 	$('h3').hide();
+// 	$('vCard').fadeIn(350);	
+// });
+
+
 	
+
+
+
+
+
+
+
+
+
+
+
